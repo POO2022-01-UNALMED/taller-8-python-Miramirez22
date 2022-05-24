f6eac52c27a2fb8b5200519ecd168a4bf9899e92
Miramirez22 <miramirez@unal.edu.co>
2022-05-24 12:39:58 -0500

    Update futbolista.py

--- futbolista.py
+++ futbolista.py
@@ -11,31 +11,24 @@ class Futbolista(Persona,Deportista):
         self._piernaHabil = piernaHabil
         Futbolista._listaFutbolistas.append(self)
 
-    def setGolesMarcados(self,goles):
-        self._golesMarcados = goles
-        
     def getGolesMarcados(self):
         return self._golesMarcados
-        
-    def setTarjetasRojas(self,rojas):
-        self._tarjetasRojas = rojas
-        
     def getTarjetasRojas(self):
         return self._tarjetasRojas
-        
-    def setPiernaHabil(self,pierna):
-        self._piernaHabil = pierna
-        
     def getPiernaHabil(self):
         return self._piernaHabil
-        
-    @classmethod
-    def setListaFutbolistas(cls,lista):
-        cls._listaFutbolistas = lista
-    
     @classmethod
     def getListaFutbolistas(cls):
         return cls._listaFutbolistas
+    def setGolesMarcados(self,nombre):
+        self._golesMarcados=nombre
+    def setTarjetasRojas(self,nombre):
+        self._tarjetasRojas=nombre
+    def setPiernaHabil(self,nombre):
+        self._piernaHabil=nombre
+    @classmethod
+    def setListaFutbolistas(cls,nombre):
+        cls._listaFutbolistas=nombre
     
     def __str__(self):
         return "Mi nombre es "+ str(self.getNombre())+" soy profesional en el deporte "+str(self.getDeporte())+" Tengo "+str(self.getEdad())+" años de edad y llevo "+str(self.getAñosPracticando())+" años en el deporte"
