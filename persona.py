e6fc31a1c6eb5b849089dc36662024dcabba37a7
Miramirez22 <miramirez@unal.edu.co>
2022-05-24 11:38:59 -0500

    Create persona.py

---
+++ persona.py
@@ -0,0 +1,30 @@
+class Persona:
+    def __init__(self,nombre,altura,edad,sexo):
+        self._nombre = nombre
+        self._edad = edad
+        self._sexo = sexo
+        self._altura=altura
+    
+    def setNombre(self,nombre):
+        self._nombre = nombre
+    
+    def getNombre(self):
+        return self._nombre
+    
+    def setEdad(self,edad):
+        self._edad = edad
+    
+    def getEdad(self):
+        return self._edad
+    
+    def setSexo(self,sexo):
+        self._sexo = sexo
+    
+    def getSexo(self):
+        return self._sexo
+    
+    def setAltura(self,altura):
+        self._altura=altura
+    
+    def getAltura(self):
+        return self._altura
